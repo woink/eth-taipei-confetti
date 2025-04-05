5f6bc9d8e437d63b34e5d86a8cdad34f46b3cdae
Ward Price <contact@wardprice.dev>
2025-04-06 02:55:14 +0800

    feat: update BuyTokenForm and swap API for improved purchase handling and error logging

--- app/api/swap/route.ts
+++ app/api/swap/route.ts
@@ -47,6 +47,7 @@ export async function GET(
   request: NextRequest
 ): Promise<NextResponse> {
 
+  console.log('get');
   return NextResponse.json({ message: "empty" });
 }
 
@@ -54,41 +55,34 @@ export async function POST(
   request: NextRequest
 ): Promise<NextResponse> {
   // Parse request body
+  console.log('post');
   let body;
   try {
-
     body = await request.json();
   } catch (error) {
-    console.error("Error parsing JSON:", error);
-    return NextResponse.json({ error: "Invalid JSON in request body" }, { status: 400 });
+    console.error('Error parsing JSON:', error);
+    return NextResponse.json({ error: 'Invalid JSON in request body' }, { status: 400 });
   }
 
-  const { 
-    srcChainId, 
-    dstChainId, 
-    srcTokenAddress, 
-    dstTokenAddress, 
-    amount 
-  } = body;
+  const { srcChainId, dstChainId, srcTokenAddress, dstTokenAddress, amount } = body;
 
   // Validate required parameters
   if (!srcChainId || !dstChainId || !srcTokenAddress || !dstTokenAddress || !amount) {
-    return NextResponse.json({ error: "Missing required parameters" }, { status: 400 });
+    return NextResponse.json({ error: 'Missing required parameters' }, { status: 400 });
   }
 
   // RPC address
-  let nodeUrl:string
+  let nodeUrl: string;
 
-  if (srcChainId == NetworkEnum.OPTIMISM){
+  if (srcChainId == NetworkEnum.OPTIMISM) {
     // @ts-ignore:next-line
     nodeUrl = process.env.RPC_URL_OP;
-  } else if (srcChainId == NetworkEnum.ARBITRUM){
+  } else if (srcChainId == NetworkEnum.ARBITRUM) {
     // @ts-ignore:next-line
     nodeUrl = process.env.RPC_URL_ARBITRUM;
   } else {
-    throw new Error("Unsupported chain ID");
+    throw new Error('Unsupported chain ID');
   }
-
 
   // This is a test account, no funds there
   // @ts-ignore:next-line
@@ -96,17 +90,17 @@ export async function POST(
   // @ts-ignore:next-line
   const makerPrivateKey: string = process.env.PRIVATE_KEY;
 
-  console.log("makerPrivateKey", makerPrivateKey);
-  console.log("makerAddress", makerAddress);
+  console.log('makerPrivateKey', makerPrivateKey);
+  console.log('makerAddress', makerAddress);
 
   // What chain is this, I'd assume the source?
 
   const blockchainProvider = new PrivateKeyProviderConnector(makerPrivateKey, new Web3(nodeUrl));
 
   const sdk = new SDK({
-    url: "https://api.1inch.dev/fusion-plus",
+    url: 'https://api.1inch.dev/fusion-plus',
     authKey: process.env.ONE_INCH_API_KEY,
-    blockchainProvider
+    blockchainProvider,
   });
 
   const params: QuoteParams = {
@@ -116,7 +110,7 @@ export async function POST(
     dstTokenAddress,
     amount,
     walletAddress: makerAddress,
-    enableEstimate: true
+    enableEstimate: true,
   };
 
   // TODO !!!!!! CHANGE RPC ADDRESS DEPENDING ON CHAIN ID
@@ -125,14 +119,15 @@ export async function POST(
   const spenderAddress = '0x111111125421ca6dc452d289314280a0f8842a65'; // aggregation router v6
   const allowance = BigInt(await tkn.allowance(makerAddress, spenderAddress)); // Ensure allowance is a BigInt
 
-  if (allowance < (2n**256n - 1n)) { // Check if allowance is less than unlimited
+  if (allowance < 2n ** 256n - 1n) {
+    // Check if allowance is less than unlimited
     await tkn.approve(
-        spenderAddress,
-        (2n**256n - 1n) // unlimited allowance
+      spenderAddress,
+      2n ** 256n - 1n // unlimited allowance
     );
-    console.log("Added more allowance");
+    console.log('Added more allowance');
   } else {
-    console.log("Sufficient allowance already exists");
+    console.log('Sufficient allowance already exists');
   }
 
   const quote: Quote = await sdk.getQuote(params);
@@ -140,22 +135,23 @@ export async function POST(
   const secretsCount = quote.getPreset().secretsCount;
 
   const secrets = Array.from({ length: secretsCount }).map(() => getRandomBytes32());
-  const secretHashes = secrets.map(x => HashLock.hashSecret(x));
+  const secretHashes = secrets.map((x) => HashLock.hashSecret(x));
 
-  const hashLock = secretsCount === 1
-    ? HashLock.forSingleFill(secrets[0])
-    : HashLock.forMultipleFills(
-      secretHashes.map((secretHash, i) =>
-        solidityPackedKeccak256(['uint64', 'bytes32'], [i, secretHash.toString()])
-      )
-    );
+  const hashLock =
+    secretsCount === 1
+      ? HashLock.forSingleFill(secrets[0])
+      : HashLock.forMultipleFills(
+          secretHashes.map((secretHash, i) =>
+            solidityPackedKeccak256(['uint64', 'bytes32'], [i, secretHash.toString()])
+          )
+        );
 
   try {
     const quoteResponse = await sdk.placeOrder(quote, {
       walletAddress: makerAddress,
       hashLock,
-      secretHashes
-    })
+      secretHashes,
+    });
 
     const orderHash = quoteResponse.orderHash;
 
@@ -163,25 +159,28 @@ export async function POST(
 
     while (true) {
       console.log(`Polling for fills until order status is set to "executed"...`);
-      await new Promise(resolve => setTimeout(resolve, 10000));
+      await new Promise((resolve) => setTimeout(resolve, 10000));
 
       try {
-        const order = await sdk.getOrderStatus(orderHash)
+        const order = await sdk.getOrderStatus(orderHash);
         if (order.status === 'executed') {
           console.log(`Order is complete. Exiting.`);
-          return NextResponse.json({ message: "worked", orderHash });
+          return NextResponse.json({ message: 'worked', orderHash });
         }
       } catch (error) {
-        console.error(`Error: ${JSON.stringify(error, null, 2)}`)
+        console.error(`Error: ${JSON.stringify(error, null, 2)}`);
       }
 
       try {
-        const fillsObject = await sdk.getReadyToAcceptSecretFills(orderHash)
+        const fillsObject = await sdk.getReadyToAcceptSecretFills(orderHash);
         if (fillsObject.fills.length > 0) {
-          fillsObject.fills.forEach(fill => {
-            sdk.submitSecret(orderHash, secrets[fill.idx])
+          fillsObject.fills.forEach((fill) => {
+            sdk
+              .submitSecret(orderHash, secrets[fill.idx])
               .then(() => {
-                console.log(`Fill order found! Secret submitted: ${JSON.stringify(secretHashes[fill.idx], null, 2)}`);
+                console.log(
+                  `Fill order found! Secret submitted: ${JSON.stringify(secretHashes[fill.idx], null, 2)}`
+                );
               })
               .catch((error) => {
                 console.error(`Error submitting secret: ${JSON.stringify(error, null, 2)}`);
@@ -193,7 +192,7 @@ export async function POST(
           console.error('Error getting ready to accept secret fills:', {
             status: error.response.status,
             statusText: error.response.statusText,
-            data: error.response.data
+            data: error.response.data,
           });
         } else if (error.request) {
           console.error('No response received:', error.request);
@@ -204,8 +203,8 @@ export async function POST(
     }
   } catch (error) {
     console.dir(error, { depth: null });
-    return NextResponse.json({ error: "Failed to process swap" }, { status: 500 });
+    return NextResponse.json({ error: 'Failed to process swap' }, { status: 500 });
   }
 
-  return NextResponse.json({ message: "worked" });
+  return NextResponse.json({ message: 'worked' });
 }
--- components/BuyTokenForm.tsx
+++ components/BuyTokenForm.tsx
@@ -5,7 +5,7 @@ import { Token } from '@/types';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import Image from 'next/image';
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import {
   Dialog,
   DialogContent,
@@ -45,10 +45,13 @@ export function BuyTokenForm({ token }: BuyTokenFormProps) {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setIsConfirmOpen(true);
-
+    // await new Promise((resolve) => setTimeout(resolve, 2000));
     try {
-      const response = await fetch('/api/buy', {
+      const response = await fetch('/api/swap', {
         method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
         body: JSON.stringify({
           srcTokenAddress: '0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85',
           dstTokenAddress: '0x912CE59144191C1204E64559FE8253a0e49E6548',
@@ -58,10 +61,13 @@ export function BuyTokenForm({ token }: BuyTokenFormProps) {
         }),
       });
       if (!response.ok) {
-        throw new Error('Purchase failed');
+        console.error('Purchase failed');
       }
-      setIsConfirmOpen(true);
+      setIsConfirmOpen(false);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      // setTimeout(() => {
       confettiRef.current?.fire();
+      // }, 500);
     } catch (error) {
       console.error('Purchase failed:', error);
     }
@@ -95,7 +101,7 @@ export function BuyTokenForm({ token }: BuyTokenFormProps) {
               }}
               type="number"
               placeholder="0.00"
-              className="text-right"
+              className="mb-2 text-right"
               min="0"
               step="0.000000000000000001"
             />
@@ -114,9 +120,13 @@ export function BuyTokenForm({ token }: BuyTokenFormProps) {
               </span>
             </div>
           </div>
-          {/* <div>
-            <Confetti ref={confettiRef} className="absolute left-0 top-0 z-0 size-full" />
-          </div> */}
+          <div>
+            <Confetti
+              ref={confettiRef}
+              manualstart={true}
+              className="pointer-events-none absolute left-0 top-0 size-full"
+            />
+          </div>
           <Button type="submit" className="mt-4 w-full">
             Buy {token.symbol}
           </Button>
